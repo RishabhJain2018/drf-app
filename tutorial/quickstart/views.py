9fd096a80d7107e3f14eb9e80275ee4af163b6ee
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-07-10 01:25:26 +0530

    PEP8 fixes

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from quickstart.serializers import UserSerializer, GroupSerializer
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -25,6 +25,7 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/', include('rest_framework.urls', 
+        namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
 ]
